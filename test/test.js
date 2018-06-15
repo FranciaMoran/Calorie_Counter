2be8ddc4f4d999e0395f4389b539adc0ae4a4b12
Francia Moran <franciam115@gmail.com>
2018-06-15 16:57:07 -0500

    first commit

---
+++ test/test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const {app, runServer, closeServer} = require('../server');
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+ 
+describe('localhost:3000', function () {
+   before(function() {
+    return runServer();
+  });
+
+   after(function() {
+    return closeServer();
+  });
+  it('should return greeting page', function () {
+    return chai.request(app)
+      .get('/')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.html;
+      });
+  });
+});
